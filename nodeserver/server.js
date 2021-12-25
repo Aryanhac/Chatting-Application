43e7a22bb2c71fa25d594287ab0bc09c88d6d4b8
Aryan Gupta <202051038@iiitvadodara.ac.in>
2021-12-25 11:52:11 +0530

    chatting part has completed by using socket.io. now anyone can send message

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -3,14 +3,20 @@ const io=require('socket.io')(8000,{cors:{origin:'*',}});  // there we are hirin
 
 const user={};
 
-io.on('connection', socket=>{
-    socket.on('new-user',name=>{
+io.on('connection', socket=>{                // it is for connect everyone
+    socket.on('new-user',name=>{             // it is for give the update to the other users about message and joining
         console.log("user name: " +name);
         user[socket.id]=name;
+        console.log(socket.id);
         socket.broadcast.emit('new-user-joined',name);
     });
     
     socket.on('send',message=>{
         socket.broadcast.emit('receive',{message: message,name:user[socket.id]})
     });
+
+    socket.on('disconnect',message=>{
+        socket.broadcast.emit('left',user[socket.id]);
+        delete user[socket.id];
+    })
 })
--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-const socket= io('http://localhost:8000');
-
+const socket= io('http://localhost:8000');            // to connect to the server
 const names=prompt('Enter Your Name');
-
 socket.emit('new-user', names);
+
+const messageContainer=document.querySelector('.box');
+const form=document.getElementById('send-container');
+
+const append1=(message)=>{
+    const messageElement=document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('rightbox');
+    messageContainer.append(messageElement);
+}
+const append2=(message)=>{
+    const messageElement=document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('leftbox');
+    messageContainer.append(messageElement);
+}
+let mess;
+function getValue(){
+    mess=document.getElementById('send').value;
+}
+
+socket.on('new-user-joined',data=>{
+    append1(`${data} joined the chat`);
+})
+
+form.addEventListener('submit',(e)=>{
+      e.preventDefault();
+      let mess=document.getElementById('input').value;
+      append1(`You:${mess}`);
+      socket.emit('send',mess);
+      document.getElementById('input').value=null;
+})
+socket.on('receive',data=>{
+    append2(`${data.name}: ${data.message}`)
+})
+
+socket.on('left',user=>{
+    append2(`${user} has left`);
+})
